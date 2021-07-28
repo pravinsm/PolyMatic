f83ca4efde95ef3dae8e2cafa7c92d7cdc8301ba
pravin sm <pravinsm@netart.io>
2021-07-28 15:12:58 +0530

    code fixed!

--- src/FormMUI.js
+++ src/FormMUI.js
@@ -183,9 +183,9 @@ const useStyles = makeStyles((theme) => ({
       "&:hover": {
         border: "2px solid #3584a7 !important",
       },
-      "& > div:nth-of-type(1)": {
-        height: "30px",
-      },
+      //   "& > div:nth-of-type(1)": {
+      //     height: "30px",
+      //   },
     },
   },
   formSubmit: {
